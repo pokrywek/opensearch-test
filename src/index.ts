5ef0b690ea85c465bdb65deee7fbe944ce7ecc57
Krzysztof Kuncki <kkuncki@craftware.biz>
2020-04-23 12:23:04 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-// import express from 'express';
-// import * as config from '../config.json';
-// import { logger } from './logger';
-// import { descriptionHandler, searchHandler } from './handlers';
+import express from 'express';
+import * as config from '../config.json';
+import { logger } from './logger';
+import { descriptionHandler, searchHandler } from './handlers';
 
-// const app = express();
-// const port = config.port;
+const app = express();
+const port = config.port;
 
-// app.use(logger());
-// app.get("/description", descriptionHandler(config.baseUrl));
-// app.get("/search", searchHandler(config.baseUrl));
-// app.all('*', (_, res) => {
-//   res
-//     .status(404)
-//     .send("Not founds");
-// });
+app.use(logger());
+app.get("/description", descriptionHandler(config.baseUrl));
+app.get("/search", searchHandler(config.baseUrl));
+app.all('*', (_, res) => {
+  res
+    .status(404)
+    .send("Not founds");
+});
 
-// app.listen(port, () => {
-//   console.log(`Server started at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
 console.log('hello');
